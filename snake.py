a082e0f58ae851754ca8e074ae5c87c636004096
A01661651 <105233241+A01661651@users.noreply.github.com>
2023-05-11 15:05:03 -0600

    Parte 1 Snake (DONE)

--- snake.py
+++ snake.py
@@ -16,21 +16,41 @@ from freegames import square, vector
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+aimf= vector(0, 1)
 
 
 def change(x, y):
-    """Change snake direction."""
+    """CCambia la dirección de la Serpiente."""
     aim.x = x
     aim.y = y
 
 
+def changef(x1,y1):
+    aimf.x=x1
+    aimf.y=y1
+
+
+def moveFood():
+    """Se define el rango, y cada vez que sea 0 el número aleatorio, procederá el if el cual hará que la comida se mueva aleatoriamente"""
+    if randrange(50)==0:        
+        x=randrange(-150,150)
+        y=randrange(-150,150)
+
+        """Aparecera la comida en el puesto aleatorio que se le asigno arriba"""
+        food = vector(x,y)      
+
+
 def inside(head):
-    """Return True if head inside boundaries."""
+    """Regresa True si la cabeza esta dentro de los límites"""
     return -200 < head.x < 190 and -200 < head.y < 190
+
+def inside(food):
+    """Parte para revisar si la comida está dentro de la pantalla"""
+    return -200 < food.x < 190 and -200 < food.y < 190
 
 
 def move():
-    """Move snake forward one segment."""
+    """Mueve la serpiente un segmento."""
     head = snake[-1].copy()
     head.move(aim)
 
@@ -40,6 +60,29 @@ def move():
         return
 
     snake.append(head)
+    #############################################################################
+    #Parte para hacer que la comida se mueva paso a paso 
+    if head != food:
+        if randrange(20)==0:
+            changef(5,0)
+        if randrange(20)==0:
+            changef(-5,0)
+        if randrange(20)==0:
+            changef(0,5)
+        if randrange(20)==0:
+            changef(0,-5)
+        food.move(aimf)
+        if not inside(food):
+            if food.x<-200:
+                food.x=190
+            if food.x>190:
+                food.x=-200
+            if food.y<-200:
+                food.y=190
+            if food.y>190:
+                food.y=-200
+
+############################################################################
 
     if head == food:
         print('Snake:', len(snake))
